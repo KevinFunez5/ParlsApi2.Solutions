32ccdeec36b54479b399db9cc31ca9138ca8c022
Kevin Funez <kevin.funez315@gmail.com>
2021-11-13 09:25:07 -0800

    add migrations

---
+++ ParksAPI2/Migrations/20211113172316_Initial.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksApi2.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ParkName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Size = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Description = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Description", "ParkName", "Size" },
+                values: new object[,]
+                {
+                    { 1, "Music, river, events", "Drake Park", "Medium" },
+                    { 2, "Gazebos, playground", "Esther Short", "Medium" },
+                    { 3, "Music, Dancing, Trees", "Mount Tabor", "Large" },
+                    { 4, "Beach, pavilions, grills", "Cottonwood", "Small" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
